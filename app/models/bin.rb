ce25b9f2f54d774d7748cc7178f7f26ac3a4ea12
Dave Lyon <dev+davelyon@hashrocket.com>
2011-05-13 09:58:02 -0500

    Shorten secret_hash, fix bookmarklet story
    [finishes #13107043, #13372023]

--- app/models/bin.rb
+++ app/models/bin.rb
@@ -1,4 +1,4 @@
-require 'digest/sha2'
+require 'digest/sha1'
 class Bin < ActiveRecord::Base
   has_many :links, order: 'created_at DESC'
 
@@ -15,6 +15,6 @@ class Bin < ActiveRecord::Base
   private
 
   def generate_hash
-    self.secret_hash = Digest::SHA2.hexdigest("#{title}--#{Time.now}")
+    self.secret_hash = Digest::SHA1.hexdigest("#{title}--#{Time.now}")
   end
 end
--- features/visitor_adds_link_to_bin_via_bookmarklet.feature
+++ features/visitor_adds_link_to_bin_via_bookmarklet.feature
@@ -4,5 +4,4 @@ Feature: Visitor adds link to bin via bookmarklet
     Given 1 bin
     When I am on the bin show page
     And I follow "Bookmarklet"
-    Then I should see "Link Saved" within "#byus_link_saver"
-    And the bin should have that link
+    Then the bin should have that link
