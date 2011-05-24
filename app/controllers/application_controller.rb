f34cf8098ebb01fe1de7bbf97507657539fdd60e
Dave Lyon <dev+davelyon@hashrocket.com>
2011-05-24 16:29:42 -0500

    Add redirection to this.isby.us for production
    [finishes #13781903]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :ensure_domain
+
+  APP_DOMAIN = 'this.isby.us'
+
+  def ensure_domain
+    return unless ::Rails.env.production?
+    if request.env['HTTP_HOST'] != APP_DOMAIN
+      redirect_to "http://#{APP_DOMAIN}#{request.env['PATH_INFO']}", status: 301
+    end
+  end
 end
